8e470039387f4d7662f496ce03f9e4ca11432dee
OLUJOHN2 <olumoyepelumi1@gmail.com>
2026-07-21 15:26:33 +0100

    mongoose course completed

---
+++ Mongoose/app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import connectDB from "./db/connectdb.js";
+import {
+  allDocs,
+  singleDoc,
+  getDocById,
+  docWithField,
+  getLimitedDoc,
+  skipDoc,
+  countDoc,
+  sortDoc,
+  operato,
+} from "./models/Movie.js";
+const app = express();
+const port = process.env.PORT || 8000;
+const DATABASE_URL =
+  process.env.DATABASE_URL || "mongodb://127.0.0.1:27017/schooldb";
+
+connectDB(DATABASE_URL);
+
+// Create & Save Document
+createDoc();
+
+insertManyDocs();
+
+// allDocs();
+// getSingleDoc();
+// getDocById();
+// docWithField();
+// getLimitedDoc();
+// skipDoc();
+// countDoc();
+// sortDoc();
+// operato();
+
+// updateByID();
+// updateOne("649ab23e38eecfddee75ea63");
+updateMany();
+
+// deleteOne("649ab23e38eecfddee75ea65");
+deleteMany();
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
